d5a961f0027f9e5d410e7a3d1c499e055857b909
Artur Morais Brasileiro <arturbrasileiro00@gmail.com>
2025-05-30 19:48:42 -0300

    Adiciona base para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/jogo-de-ingles/',    // <— aqui
   server: {
     fs: {
       strict: false
